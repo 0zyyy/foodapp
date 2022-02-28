4dea2eb05621ce58471f2466b5983f04ede2008c
0zyyy <jalufazlurrahman@gmail.com>
2022-02-28 08:16:55 +0700

    Revisi halaman popular food detail

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:foodapp/utils/colors.dart';
 import 'package:foodapp/utils/dimension.dart';
+import 'package:foodapp/widgets/app_column.dart';
 import 'package:foodapp/widgets/app_icon.dart';
 import 'package:foodapp/widgets/big_text.dart';
+import 'package:foodapp/widgets/expandable_text.dart';
+import 'package:foodapp/widgets/small_text.dart';
 
 class PopularFoodPageDetail extends StatelessWidget {
   const PopularFoodPageDetail({Key? key}) : super(key: key);
@@ -9,6 +13,7 @@ class PopularFoodPageDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(children: [
         Positioned(
           left: 0,
@@ -39,6 +44,7 @@ class PopularFoodPageDetail extends StatelessWidget {
           top: Dimensions.PopularImgSize - 20.0,
           left: 0,
           right: 0,
+          bottom: 0,
           child: Container(
             padding: EdgeInsets.only(
                 left: Dimensions.width20,
@@ -54,16 +60,83 @@ class PopularFoodPageDetail extends StatelessWidget {
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [BigText(text: 'Popular Junk Food')],
+              children: [
+                AppColumn(text: 'Rendang Balado'),
+                SizedBox(height: Dimensions.height20),
+                BigText(text: 'Kenalan Dong Bund'),
+                SizedBox(height: Dimensions.height10),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: ExpandableText(
+                        text:
+                            'Makanan ini sangat enak dan bisa dikonsumsi sehari-hari. Makanan ini sangat enak dan bisa dikonsumsi sehari-hari. Makanan ini sangat enak dan bisa dikonsumsi sehari-hari. Makanan ini sangat enak dan bisa dikonsumsi sehari-hari. Makanan ini sangat enak dan bisa dikonsumsi sehari-hari. Makanan ini sangat enak dan bisa dikonsumsi sehari-hari. Makanan ini sangat enak dan bisa dikonsumsi sehari-hari.'),
+                  ),
+                ),
+              ],
             ),
           ),
         )
-        // Container(
-        //   width: double.maxFinite,
-        //   height: 350,
-        //   decoration: BoxDecoration(color: Colors.black),
-        // )
       ]),
+      bottomNavigationBar: Container(
+        height: Dimensions.NavbarHeight,
+        padding: EdgeInsets.only(
+            top: Dimensions.height30,
+            bottom: Dimensions.height30,
+            left: Dimensions.width20,
+            right: Dimensions.width20),
+        decoration: BoxDecoration(
+          color: AppColors.buttonBackgroundColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(Dimensions.radius20 * 2),
+            topRight: Radius.circular(Dimensions.radius20 * 2),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+                padding: EdgeInsets.only(
+                    top: Dimensions.height20,
+                    bottom: Dimensions.height20,
+                    right: Dimensions.width20,
+                    left: Dimensions.width20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(Dimensions.radius20),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.remove,
+                      color: AppColors.signColor,
+                    ),
+                    SizedBox(width: Dimensions.width10 / 2),
+                    BigText(text: '0'),
+                    SizedBox(width: Dimensions.width10 / 2),
+                    Icon(
+                      Icons.add,
+                      color: AppColors.signColor,
+                    )
+                  ],
+                )),
+            Container(
+              padding: EdgeInsets.only(
+                  top: Dimensions.height20,
+                  bottom: Dimensions.height20,
+                  right: Dimensions.width20,
+                  left: Dimensions.width20),
+              decoration: BoxDecoration(
+                color: AppColors.mainColor,
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+              ),
+              child: BigText(
+                text: 'Add to Cart',
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
