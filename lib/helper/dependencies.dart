0a972ed61061eac48d324a9bfc8577a72ed75466
0zyyy <jalufazlurrahman@gmail.com>
2022-03-01 20:30:36 +0700

    init utk RekomProduk

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,13 +1,18 @@
 import 'package:foodapp/controllers/populer_product_controllers.dart';
+import 'package:foodapp/controllers/rekom_product_controllers.dart';
 import 'package:foodapp/data/api/api_client.dart';
 import 'package:foodapp/data/repository/popular_product_repo.dart';
+import 'package:foodapp/data/repository/rekom_product_repo.dart';
+import 'package:foodapp/utils/app_const.dart';
 import 'package:get/instance_manager.dart';
 
 Future<void> init() async {
   //LOAD API CLIENT
-  Get.lazyPut(() => ApiClient(appBaseUrl: 'https://dbestech.com'));
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstatns.BASE_URL));
   //LOAD REPOSOTORY
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+  Get.lazyPut(() => RekomProdukRepo(apiClient: Get.find()));
   //LOAD CONTROLLERS
   Get.lazyPut(() => PopulerProductControllers(popularProductRepo: Get.find()));
+  Get.lazyPut(() => RekomProdukController(rekomProdukRepo: Get.find()));
 }
