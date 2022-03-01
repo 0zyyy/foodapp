0696dc63353ad784e0ccf06b1ab0555d826ca053
0zyyy <jalufazlurrahman@gmail.com>
2022-03-01 20:32:58 +0700

    Membuat app const agar dinamis

---
+++ lib/utils/app_const.dart
@@ -0,0 +1,9 @@
+class AppConstatns {
+  static const String APP_NAME = "Flutter Demo";
+  static const String BASE_URL = 'https://mvs.bslmeiyu.com';
+  static const String POPULAR_PRODUCT = '/api/v1/products/popular';
+  static const String RECOMMEND_PRODUCT = '/api/v1/products/recommended';
+
+  //TOKEN
+  static const String TOKEN = 'DBtoken';
+}
