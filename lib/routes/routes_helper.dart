903defae15e02031ba958b6c01caa7e4911f8279
0zyyy <jalufazlurrahman@gmail.com>
2022-03-02 18:16:18 +0700

    Menambah routes baru untuk halaman populer,rekom

--- lib/routes/routes_helper.dart
+++ lib/routes/routes_helper.dart
@@ -1,15 +1,37 @@
 import 'package:foodapp/pages/food/popular_food_detail.dart';
+import 'package:foodapp/pages/food/recom_food_detail.dart';
 import 'package:foodapp/pages/home/main_page.dart';
 import 'package:get/get.dart';
 
 class RoutesHelper {
   static const String home = '/';
-  static const String popular = '/popular';
-  List<GetPage> routes = [
+  static const String popular = '/popular-food';
+  static const String rekomen = '/rekomen-food';
+
+  static String getInitial() => '$home';
+  static String getPopularPage(int pageId) => '$popular?pageId=$pageId';
+  static String getRekomenPage(int pageId) => '$rekomen?pageId=$pageId';
+
+  static List<GetPage> routes = [
     GetPage(
       name: home,
       page: () => MainPage(),
     ),
-    GetPage(name: popular, page: () => PopularFoodPageDetail()),
+    GetPage(
+        name: popular,
+        page: () {
+          var pageId = Get.parameters['pageId'];
+          return PopularFoodPageDetail(pageId: int.parse(pageId!));
+        },
+        transition: Transition.fadeIn),
+    GetPage(
+        name: rekomen,
+        page: () {
+          var pageId = Get.parameters['pageId'];
+          return RecomDetail(
+            pageId: int.parse(pageId!),
+          );
+        },
+        transition: Transition.fadeIn),
   ];
 }
