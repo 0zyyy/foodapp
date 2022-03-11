937a03b091728595e1ecf88dc8ae2352091f1aa4
0zyyy <jalufazlurrahman@gmail.com>
2022-03-11 09:53:28 +0700

    lazyput for cart controller and repo

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,6 +1,8 @@
+import 'package:foodapp/controllers/cart_product_controller.dart';
 import 'package:foodapp/controllers/populer_product_controllers.dart';
 import 'package:foodapp/controllers/rekom_product_controllers.dart';
 import 'package:foodapp/data/api/api_client.dart';
+import 'package:foodapp/data/repository/cart_product_repo.dart';
 import 'package:foodapp/data/repository/popular_product_repo.dart';
 import 'package:foodapp/data/repository/rekom_product_repo.dart';
 import 'package:foodapp/utils/app_const.dart';
@@ -12,7 +14,9 @@ Future<void> init() async {
   //LOAD REPOSOTORY
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
   Get.lazyPut(() => RekomProdukRepo(apiClient: Get.find()));
+  Get.lazyPut(() => CartRepo());
   //LOAD CONTROLLERS
   Get.lazyPut(() => PopulerProductControllers(popularProductRepo: Get.find()));
   Get.lazyPut(() => RekomProdukController(rekomProdukRepo: Get.find()));
+  Get.lazyPut(() => CartController(cartRepo: Get.find()));
 }
