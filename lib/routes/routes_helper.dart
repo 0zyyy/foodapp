a59610931008af614f84ebfba20aa755f3112dfe
0zyyy <jalufazlurrahman@gmail.com>
2022-03-12 08:55:25 +0700

    Added route to cart page

--- lib/routes/routes_helper.dart
+++ lib/routes/routes_helper.dart
@@ -1,3 +1,4 @@
+import 'package:foodapp/pages/cart/cart_pages.dart';
 import 'package:foodapp/pages/food/popular_food_detail.dart';
 import 'package:foodapp/pages/food/recom_food_detail.dart';
 import 'package:foodapp/pages/home/main_page.dart';
@@ -7,10 +8,14 @@ class RoutesHelper {
   static const String home = '/';
   static const String popular = '/popular-food';
   static const String rekomen = '/rekomen-food';
+  static const String cartPage = '/cart-page';
 
   static String getInitial() => '$home';
-  static String getPopularPage(int pageId) => '$popular?pageId=$pageId';
-  static String getRekomenPage(int pageId) => '$rekomen?pageId=$pageId';
+  static String getPopularPage(int pageId, String page) =>
+      '$popular?pageId=$pageId&page=$page';
+  static String getRekomenPage(int pageId, String page) =>
+      '$rekomen?pageId=$pageId&page=$page';
+  static String getCartPage() => '$cartPage';
 
   static List<GetPage> routes = [
     GetPage(
@@ -18,18 +23,28 @@ class RoutesHelper {
       page: () => MainPage(),
     ),
     GetPage(
+      name: cartPage,
+      page: () {
+        return CartPage();
+      },
+      transition: Transition.fadeIn,
+    ),
+    GetPage(
         name: popular,
         page: () {
           var pageId = Get.parameters['pageId'];
-          return PopularFoodPageDetail(pageId: int.parse(pageId!));
+          var page = Get.parameters['page'];
+          return PopularFoodPageDetail(pageId: int.parse(pageId!), page: page!);
         },
         transition: Transition.fadeIn),
     GetPage(
         name: rekomen,
         page: () {
           var pageId = Get.parameters['pageId'];
+          var page = Get.parameters['page'];
           return RecomDetail(
             pageId: int.parse(pageId!),
+            page: page!,
           );
         },
         transition: Transition.fadeIn),
