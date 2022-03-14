d6cc512ac496275afbe19e06eae3cc080dca6445
0zyyy <jalufazlurrahman@gmail.com>
2022-03-14 14:02:32 +0700

    Added method to convert object to json

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -65,4 +65,16 @@ class ProductModel {
     updatedAt = json['updated_at'];
     typeId = json['type_id'];
   }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price,
+      'img': img,
+      'location': location,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
+      'type_id': typeId,
+    };
+  }
 }
